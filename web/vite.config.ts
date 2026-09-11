bcf5d5f1b4434a3ee5f0cedf31bd75818554b4e9
Dakota Smith <zombieslayer9er@gmail.com>
2026-09-11 16:50:33 -0400

    Use standard Sites build output

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: { outDir: '../dist', emptyOutDir: true },
   server: {
     port: 5173,
     proxy: {
